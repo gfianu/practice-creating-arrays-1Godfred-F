0bd527f549173a20145751be936431b4f585a9cc
gfianu <delyouandme@gmail.com>
2025-07-14 10:00:57 -0400

    Creating arrays

--- creatingArrays.js
+++ creatingArrays.js
@@ -1,1 +1,21 @@
-
+/*
+Tasks
+Task 1: Create Arrays
+Create three separate arrays using array literals for the following:
+● An array of 5 grocery store items.
+● An array of 3 numbers representing prices.
+Task 2: Access and Log the Values
+Use console.log() to log the arrays.
+1. Log the entire array of grocery items.
+2. Log the entire array of prices.
+3. Log three individual items of your choice.
+4. Log two prices of your choice.
+*/
+// Task 1: Create Arrays
+const groceryItems = ['Apples', 'Bananas', 'Carrots', 'Dairy Milk', 'Eggs'];
+const prices = [3.99, 4.99, 5.99];
+// Task 2: Access and Log the Values
+console.log(groceryItems); // Log the entire array of grocery items
+console.log(prices); // Log the entire array of prices
+console.log(groceryItems[0], groceryItems[2], groceryItems[4]); // Log three individual items of your choice
+console.log(prices[0], prices[1]); // Log two prices of your choice  
